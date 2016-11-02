d2df5f4efa3118a47d8b9c04776ff640517bb0ce
kidocean <chikwok@usc.edu>
2016-11-01 19:19:25 -0700

    submitted

---
+++ question-2.js
@@ -0,0 +1,49 @@
+// Fill out the `filterUsersByState` function so that it returns a
+// filtered list of users by the `state` variable passed in. Use the array `filter()` method.
+// Ignore the `module.exports` at the bottom. You can run this file by using
+// `nodemon question-2.js` or `node question-2.js` and seeing your results in the terminal.
+// Alternatively, you can try out your implementation somewhere else and copy your
+// solution back into this file.
+
+function filterUsersByState(users, state) {
+  var filtered = users.filter(function(user){
+     if (user.address.state == state){
+         return true;
+     } else {
+         return false;
+     }
+      
+  });
+    return filtered
+}
+
+var users = [
+  {
+    name: 'David',
+    address: {
+      state: 'CA'
+    }
+  },
+  {
+    name: 'Jeff',
+    address: {
+      state: 'TX'
+    }
+  },
+  {
+    name: 'Sandra',
+    address: {
+      state: 'NY'
+    }
+  },
+  {
+    name: 'Nevil',
+    address: {
+      state: 'CA'
+    }
+  }
+];
+
+var usersInCA = filterUsersByState(users, 'CA');
+console.log(usersInCA);
+module.exports = usersInCA;
